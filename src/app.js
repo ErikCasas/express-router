6a8eca2bba7a439dc36e378ba06583b18b3f8a23
Erik <santicasas667@gmail.com>
2023-08-29 23:29:17 -0500

    comentarios

--- src/app.js
+++ src/app.js
@@ -1,70 +1,91 @@
 const express = require("express");
-const booksRoutes = require("./routes/booksRoutes")
+const booksRoutes = require("./routes/booksRoutes");
+const users = require("./utils/users");
 
 const app = express();
 
-app.use(express.json());//linea de código que me permite acceder al body 
+//-----------------------------------------
+
+app.use(express.json()); //linea de código que me permite acceder al body
+
+//inidicamos que en todas las rutas con prefijo de "/books" use las rutas
+//que difinimos en booksRoutes.js
+
+app.use("/books", booksRoutes);
 
 
-app.use("/books", booksRoutes)
 
-app.get("/", (_req, res) => {
-  res.send("has solicitado informacion a la ruta raiz");
-});
 
-app.post("/", (_req, res) => {
-  res.send("estas enviando informacion");
-});
 
-app.put("/", (_req, res) => {
-  res.send("estas modificando informacion");
-});
 
-app.delete("/", (_req, res) => {
-  res.send("estas eliminando informacion");
-});
+//-----------------------------------------
+//ejemplo de ruta con metodos encadenados
+//-----------------------------------------
+//esto es a traves de la propiedad "route", de este modo solo debemos definir la ruta
+//y decir que metodos atendera
 
-//---------------------------------------------------
-//---------------------------------------------------
-
+//BUENA PRACTICA: en caso de no tener que usar algo pero si debe estar ahi, poner al inicio de este "_"
+//como con "_req"
 app
   .route("/clients")
   .get((_req, res) => {
-    res.send("Estos son todos los clientes");
+    res.send("peticion de tipo GET a la ruta clients");
   })
   .post((_req, res) => {
-    res.send("estas agregando un nuevo cliente");
+    res.send("peticion de tipo POST a la ruta clients");
   })
   .put((_req, res) => {
-    res.send("estas modificando un cliente");
+    res.send("peticion de tipo PUT a la ruta clients");
   })
   .delete((_req, res) => {
-    res.send("estas eliminando un cliente");
+    res.send("peticion de tipo DELETE a la ruta clients");
   });
 
-//---------------------------------------------------
-//---------------------------------------------------
+//CUIDADO: si bien este metodo de enrutamiento optimiza nuestr código
+//en caso de que en cada metodo necesitamos de una cantidad considerable de
+//lineas de codigo, esto puedo hacer que nuestro código sea sucio y dificil de entender
+
+//-----------------------------------------
+
+//ejemplos de rutas que cumplen con el CRUD que nos ayudaran en ejemplos futuros
+//la explicacion de las rutas dinamicas esta en el archivo booksRoutes.js
+//-----------------------------------------
+
+//todos los usuarios en general
+app.get("/users",(_req,res)=>{
+    res.status(200).send(users)
+})
+
+//un usuario en especifico
+app.get("/users/:userId",(req,res)=>{
+    const id = req.params.userId
+    const index = users.findIndex((user)=>user.id==id)
+    res.status(200).send({user:users[index]})
+})
 
 
-// app.get("/books",(req,res)=>{
-//     res.send(books)
-// })
+app.post("/users",(req,res)=>{
+   const newUser = req.body
+   users.push(newUser)
+   res.status(201).send("usuario creado")
+})
+
+app.put("/users/:userId",(req,res)=>{
+   const id = req.params.userId
+   const newInfo = req.body
+   const index = users.findIndex((user)=>user.id==id)
+   users[index]=newInfo
+   res.status(200).send({modificado: users[index]})
+})
 
 
-// path params
+app.delete("/users/:userId",(req,res)=>{
+    const id = req.params.userId
+    const index = users.findIndex((user)=>user.id==id)
+    const deleteUser = users.splice(index, 1)
+    res.status(200).send({deleteUser})
+})
 
 
-// app.get("/books/:id",(req,res)=>{
-// const id = req.params.id
-//     const book = books.find((e)=>e.id==id)
-//     res.send(book)
-// })
-
-
-// app.post("/books",(req,res)=>{
-//     const newBook = req.body
-//     books.push(newBook)
-//     res.send(books)
-// })
 
 module.exports = app;
--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -1,17 +1,33 @@
-const express = require("express");
 const books = require("../utils/library");
+//------------------------------------------
+const express = require("express");
 
-const router = express.Router();
+const router = express.Router();//instancia de express router
 
+
+//definimos los metodos y en que endpoints puede atender 
+//asi como con la constante app, solo que aquí cuando hablamos de ruta raiz
+//no nos referimos "http:localhost:3000/" sino a "http:localhost:3000/books"
+//esto debido a que lo definimos en el archivo app.js
+
+//-------------------------------
 router.get("/", (req, res) => {
-  res.send(books);
-});
-router.get("/:id", (req, res) => {
-  const id = req.params.id;
-  const book = books.find((e) => e.id == id);
-  res.send(book);
+  res.status(200).send(books);
 });
 
+
+//-------------------------------
+//path params ==> ruta dinamica, es decir que podemos definir una ruta
+//con datos que pueden variar, esto nos ayuda a acceder a recursos de forma a precisa
+
+router.get("/:bookId", (req, res) => {
+  //aceddemos a ese id que llega por params a traves del objeto "res"
+  const id = req.params.bookId;//si definimos que el path param se llama bookId, accedemos a el por ese nombre
+  const book = books.find((e) => e.id == id);
+  res.status(200).send(book);
+});
+
+//-------------------------------
 router.post("/", (req, res) => {
   const newBook = req.body;
   books.push(newBook);
@@ -19,18 +35,18 @@ router.post("/", (req, res) => {
 });
 
 
-
-router.put("/:id", (req, res) => {
-    const id = req.params.id
+//-------------------------------
+router.put("/:bookId", (req, res) => {
+    const id = req.params.bookId
     const modifyBook = req.body;
     const indexBook = books.findIndex((e)=>e.id==id)
     books[indexBook] = modifyBook
     res.send(books[indexBook]);
   });
 
-
-router.delete("/:idBook", (req, res) => {
-    const id = req.params.idBook
+//-------------------------------
+router.delete("/:bookId", (req, res) => {
+    const id = req.params.bookId
     const indexBook = books.findIndex((e)=>e.id==id)
     const deleteBook = books.splice(indexBook,1)
     res.send({deleteBook})
